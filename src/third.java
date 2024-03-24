7978a4ae83b4b3d1372d46b2d72277e731d2ed0b
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-24 06:35:24 +0530

    -> Question Three Added

---
+++ src/third.java
@@ -0,0 +1,14 @@
+public class third {
+    public static void main(String[] args) {
+        int num = 5;
+
+        // pattern 3
+
+        for (int i = 0; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
