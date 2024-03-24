0f1e8a287f83b6071ac0cd46a223b0bab3cfe76c
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-24 06:38:08 +0530

    -> Question Four Added

---
+++ src/four.java
@@ -0,0 +1,14 @@
+public class four {
+    public static void main(String[] args) {
+        int num = 5;
+
+        // Pattern 4
+
+        for (int i = 1; i <= num ; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
